3ca046b50331ca4ca8413c7045906e8cf9fcf6ac
sedil64 <dilan6465@gmail.com>
2026-01-18 13:07:05 -0500

    Inserción del refugio al que pertenezca la mascota

--- pets/serializers/pet.py
+++ pets/serializers/pet.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from pets.models.pet import Pet
 
 class PetSerializer(serializers.ModelSerializer):
+    shelter_name = serializers.CharField(source='shelter.name', read_only=True)
+    
     class Meta:
         model = Pet
         fields = '__all__'
