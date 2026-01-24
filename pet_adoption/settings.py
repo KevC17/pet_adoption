543f7bf4a79a7b78e51ca7ae4eb25af685ef436f
sedil64 <dilan6465@gmail.com>
2026-01-24 14:34:36 -0500

    CORS ALLOW

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'corsheaders',
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -57,8 +58,11 @@ INSTALLED_APPS = [
     'adoption_tracking',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
