7cadcbe0c1d3f67c02bb8cd44d708da8a229a299
sedil64 <dilan6465@gmail.com>
2026-01-24 14:48:27 -0500

    CORS ALLOW

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -58,7 +58,14 @@ INSTALLED_APPS = [
     'adoption_tracking',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
