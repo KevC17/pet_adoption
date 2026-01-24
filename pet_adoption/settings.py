371088940fd19c90383fc90528154fc31c0eee86
sedil64 <dilan6465@gmail.com>
2026-01-24 15:00:16 -0500

    CORS ALLOW

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -68,8 +68,8 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
