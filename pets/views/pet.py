38c9562348b2dcb7ac4d1d6aa8519c72208778fc
sedil64 <dilan6465@gmail.com>
2026-01-25 18:49:12 -0500

    IMP: Filtro de mascotas por refugio

--- pets/views/pet.py
+++ pets/views/pet.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from pets.models.pet import Pet
 from pets.serializers.pet import PetSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -14,7 +15,16 @@ class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all().order_by('id')
     serializer_class = PetSerializer
     permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+
+    filter_backends = (
+        DjangoFilterBackend,     
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+    filterset_fields = ('shelter',)  
+
     search_fields = ('name', 'species', 'breed', 'gender', 'status')
     ordering_fields = ('name', 'age', 'admission_date')
+
     parser_classes = (MultiPartParser, FormParser)
