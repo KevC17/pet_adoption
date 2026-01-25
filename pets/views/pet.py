23282bb52655b0bf5bdec649bd58750463306d55
sedil64 <dilan6465@gmail.com>
2026-01-25 18:58:55 -0500

    IMP: Filtro de mascotas por refugio

--- pets/views/pet.py
+++ pets/views/pet.py
@@ -11,20 +11,27 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         return request.user.is_staff
 
+
 class PetViewSet(viewsets.ModelViewSet):
-    queryset = Pet.objects.all().order_by('id')
     serializer_class = PetSerializer
     permission_classes = (IsAdminOrReadOnly,)
 
     filter_backends = (
-        DjangoFilterBackend,     
+        DjangoFilterBackend,
         filters.SearchFilter,
         filters.OrderingFilter,
     )
 
-    filterset_fields = ('shelter',)  
-
+    filterset_fields = ('shelter',)
     search_fields = ('name', 'species', 'breed', 'gender', 'status')
     ordering_fields = ('name', 'age', 'admission_date')
-
     parser_classes = (MultiPartParser, FormParser)
+
+    def get_queryset(self):
+        queryset = Pet.objects.all().order_by('id')
+
+        shelter_id = self.request.query_params.get('shelter')
+        if shelter_id:
+            queryset = queryset.filter(shelter_id=shelter_id)
+
+        return queryset
