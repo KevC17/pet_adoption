5cbe9ace9d36162e099e1d2307dfd63e191e8a9d
sedil64 <dilan6465@gmail.com>
2026-01-25 20:03:29 -0500

    Fix: Add proper CORS configuration

--- shelters/views/shelter.py
+++ shelters/views/shelter.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from shelters.models.shelter import Shelter
 from shelters.serializers.shelter import ShelterSerializer
+
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -8,10 +10,20 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         return request.user.is_staff
 
+
 class ShelterViewSet(viewsets.ModelViewSet):
     queryset = Shelter.objects.all().order_by('id')
     serializer_class = ShelterSerializer
     permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+    filterset_fields = ('is_active',)
+
     search_fields = ('name', 'address', 'phone')
+
     ordering_fields = ('name',)
