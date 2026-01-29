267ae537263106df6af0e9343c370af6b218c70a
sedil64 <dilan6465@gmail.com>
2026-01-29 17:50:12 -0500

    FIX: filtro de mascotas adoptadas

--- pets/filters.py
+++ pets/filters.py
@@ -2,8 +2,11 @@ import django_filters
 from pets.models.pet import Pet
 
 class PetFilter(django_filters.FilterSet):
-    shelter = django_filters.NumberFilter(field_name='shelter__id')
-    
+    status = django_filters.CharFilter(
+        field_name='status',
+        lookup_expr='exact'
+    )
+
     class Meta:
         model = Pet
-        fields = ['shelter']
+        fields = ['status']
